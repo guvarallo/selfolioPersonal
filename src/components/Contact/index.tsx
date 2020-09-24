6c53c5d44bfbdc1284ad7bb87497dff5578e0c92
guvarallo <gustavo.varallo@live.com>
2020-09-24 20:24:51 -0300

    fixed mailto target blank

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -14,7 +14,9 @@ const Contact: React.FC = () => {
       <div>
         <Fade bottom duration={1000} delay={300} distance="30px">
           <p>{contactData.description}</p>
-          <Cta href={contactData.mailTo}>E-mail</Cta>
+          <Cta href={contactData.mailTo} target="_blank">
+            E-mail
+          </Cta>
         </Fade>
       </div>
     </Container>
