bd66a6669a2cd84c75705775bb607cf5e641eb5b
guvarallo <gustavo.varallo@live.com>
2020-10-06 10:40:16 -0300

    final touch for dark-mode

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import { colors } from '../source/data';
 
 export default createGlobalStyle`
   * {
@@ -12,6 +13,18 @@ export default createGlobalStyle`
 
   #root {
     margin: 0 auto;
+  }
+
+  body.light-mode {
+    background: ${colors.background};
+    color: #000;
+    transition: background 0.3s ease;
+  }
+
+  body.dark-mode {
+    background: ${colors.first};
+    color: ${colors.background};
+    transition: background 0.3s ease;
   }
 
   button {
